b4d3bbf770655bb0c7f38087f9912aeb57173cd3
Maryana <47738693+MaryanaYavorska@users.noreply.github.com>
2019-08-14 17:24:49 +0300

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb://user_1600:scram12345@test-cluster-shard-00-00-sjxtd.mongodb.net:27017,test-cluster-shard-00-01-sjxtd.mongodb.net:27017,test-cluster-shard-00-02-sjxtd.mongodb.net:27017/test?ssl=true&replicaSet=test-cluster-shard-0&authSource=admin&retryWrites=true&w=majority'
+}
